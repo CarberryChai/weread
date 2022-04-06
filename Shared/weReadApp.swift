cf15ff49522d18c296a1309cc7259704ac974dbb
Carberry <CarberryChai@gmail.com>
2022-04-06 14:18:24 +0800

    Initial Commit

---
+++ Shared/weReadApp.swift
@@ -0,0 +1,17 @@
+//
+//  weReadApp.swift
+//  Shared
+//
+//  Created by 柴长林 on 2022/4/6.
+//
+
+import SwiftUI
+
+@main
+struct weReadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
